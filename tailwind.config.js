c456a2ab5fdbb3558ad17de685d90c9246249a7a
hadri harazit <hadriharazit2721@gmail.com>
2025-12-28 06:03:57 +0800

    update railway

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,6 +17,6 @@ export default {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    forms,
   ],
 }
